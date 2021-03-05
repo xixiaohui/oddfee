681c2d8a22543449833c947536866c98bdcd8d9c
xixiaohui <yamazake@yeah.net>
2021-03-05 11:43:04 +0800

    let the ringtones not reloaded

--- public/js/my.js
+++ public/js/my.js
@@ -209,6 +209,53 @@ if (!currentPage) {
     console.log(typeof(currentPage));
 }
 
+let allRingtones = [];
+
+window.onload = function() {
+    // alert('load');
+    let href = window.location.href;
+
+    //在首页或者分类页面把页面加载的铃声全部在手机加载的时候加入到页面中
+    if (href.endsWith("ringtone") || href.endsWith("all")) {
+        let sessionStorageRingtones = window.sessionStorage.getItem('ringtones');
+        // console.log(sessionStorageRingtones);
+        if (sessionStorageRingtones != null) {
+
+            let results = JSON.parse(sessionStorageRingtones);
+            console.log(results);
+            allRingtones = results;
+
+            //把sessionStorage里存的铃声对象全部插入到页面
+            insertRingtoneDatatToHtml(allRingtones)
+        }
+    }
+}
+
+
+
+//处理待保存的铃声
+function adjustRingtoneDataToSessionStorage(ringtones) {
+
+    for (let index = 0; index < ringtones.length; index++) {
+        const element = ringtones[index];
+
+        allRingtones.push(element);
+    }
+}
+
+//区别手机和PC 插入从后端加载的数据
+function insertRingtoneDatatToHtml(results) {
+    //把数据添加到页面上去 只有移动端插入数据
+    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        // window.location.href="移动端url";
+        // alert("mobile");
+        insertArrayRingtones(results);
+    } else {
+        // alert("pc")
+
+    }
+}
+
 //手机上加载更多铃声
 function addLoadingData() {
     console.log("Bottom.");
@@ -227,6 +274,7 @@ function addLoadingData() {
     console.log(currentPage);
     //保存到session中
     window.sessionStorage.setItem('currentPage', currentPage);
+
     const requestUrl = '/ringtone/data?page=' + currentPage.toString();
 
     console.log(requestUrl);
@@ -238,11 +286,17 @@ function addLoadingData() {
         if (this.readyState == 4 && this.status == 200) {
 
             const results = JSON.parse(http.responseText);
-            console.log(results);
-            currentPage += 1;
+            // console.log(results);
 
-            //把数据添加到页面上去
-            insertArrayRingtones(results);
+            //把results各项保存到allRingtones
+            adjustRingtoneDataToSessionStorage(results)
+                //把allRingtones保存到sessionStorage
+            window.sessionStorage.setItem('ringtones', JSON.stringify(allRingtones));
+
+            //增加当前页
+            currentPage += 1;
+            //插入加载的铃声到页面
+            insertRingtoneDatatToHtml(results)
         }
     }
 }
@@ -253,7 +307,6 @@ function insertArrayRingtones(ringtones) {
     for (let index = 0; index < ringtones.length; index++) {
         const element = ringtones[index];
         insertOneRingtone(element);
-
     }
 }
 
