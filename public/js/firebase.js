f0454bc9b8b4f38cec83f10c9cb20b1ae4493465
xixiaohui <yamazake@yeah.net>
2021-03-09 09:01:21 +0800

    add firebase

---
+++ public/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCOfvQnMOogTmXT0YXWJuWzHE0L74ukLhI",
+    authDomain: "oddfee.firebaseapp.com",
+    projectId: "oddfee",
+    storageBucket: "oddfee.appspot.com",
+    messagingSenderId: "751518390257",
+    appId: "1:751518390257:web:3be520de3f0da8a1819650",
+    measurementId: "G-667E1D34L5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
